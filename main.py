83c6501310e1711886bed27b012d9c7569400f2a
James <38766515+Fraetor@users.noreply.github.com>
2020-02-15 17:56:20 +0000

    Some graphing shinanigans

--- main.py
+++ main.py
@@ -6,15 +6,27 @@ Created on Sat Feb 15 13:56:27 2020
 """
 import detectface as df
 import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_structure(faces):
+    x = faces[:,0]
+    y = faces[:,2]
+    return x, y
 
 
 # Get video from webcam
 video_capture = cv2.VideoCapture(0)
 
+
 while True:
     frame, grey_frame = df.get_frame(video_capture)
     faces = df.detect_face(grey_frame)
     print(faces)
+    x, y = plot_structure(faces)
+    plt.plot(x, y, "xg")
+    plt.draw()
     if not df.debug_display(frame, faces):
         video_capture.release()
         cv2.destroyAllWindows()
