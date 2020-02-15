6b89fffc6ee3d9159de1c6ea13e8063225c4b7bc
James <38766515+Fraetor@users.noreply.github.com>
2020-02-15 16:08:11 +0000

    Further clean up code

--- main.py
+++ main.py
@@ -4,15 +4,8 @@ Created on Sat Feb 15 13:56:27 2020
 
 @author: James
 """
-
+#%% Module imports
 import cv2
-
-# Import cascade 
-cascPath = "haarcascade_frontalface_default.xml"
-faceCascade = cv2.CascadeClassifier(cascPath)
-
-# Get video from webcam
-video_capture = cv2.VideoCapture(0)
 
 
 def detect_face(grey):
@@ -24,7 +17,7 @@ def detect_face(grey):
     # Detects the facs in the frame
     faces = faceCascade.detectMultiScale(
         grey,
-        scaleFactor=1.2,
+        scaleFactor=1.3,
         minNeighbors=5,
         minSize=(30, 30),
         #flags=cv2.cv.CV_HAAR_SCALE_IMAGE
@@ -34,6 +27,7 @@ def detect_face(grey):
 
 def get_frame(frame_source):
     """
+    Gets the frame from the video source and preprocesses it.
     Requires a video source as its argument.
     Returns a colour frame and a greyscale frame.
     """
@@ -44,19 +38,37 @@ def get_frame(frame_source):
     return frame, grey
 
 
+def debug_display(frame, faces):
+    """
+    Displays the current frame with the faces shown as rectangles.
+    Requires a frame and a faces object as arguments.
+    Returns False if the exit key, "q" is pressed, or True otherwise.
+    """
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+    # Exit if "q" key is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        return False
+    else:
+        return True
+    
+
+# Import cascade 
+cascPath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+# Get video from webcam
+video_capture = cv2.VideoCapture(0)
+
+
+#%% Main loop
 while True:
     frame, grey_frame = get_frame(video_capture)
     faces = detect_face(grey_frame)
-    
-    # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
-    
-    # Exit if "q" key is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    print(faces)
+    if not debug_display(frame, faces):
         break
 
 # When everything is done, release the capture
