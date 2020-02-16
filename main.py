6dce24a3f0e17e621b6bcebcceb1af5dfbd0ead3
James <38766515+Fraetor@users.noreply.github.com>
2020-02-16 10:19:10 +0000

    Fuck unity and embraces matplotlib

--- main.py
+++ main.py
@@ -5,14 +5,32 @@ Created on Sat Feb 15 13:56:27 2020
 @author: James
 """
 import detectface as df
-from datetime import datetime
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+    
+def get_angles():
+    face, eye = df.get_coordinate_pair()
+    angle_horizontal, angle_vertical = df.calculate_angles(eye, face)
+    print(angle_horizontal, angle_vertical)
+    return angle_horizontal, angle_vertical
 
-start = datetime.now()
 
-face, eye = df.get_coordinate_pair()
-angle_horizontal, angle_vertical = df.calculate_angles(eye, face)
-print(angle_horizontal, angle_vertical)
+x = np.linspace(-1, 1, 100)
+y = np.linspace(-1, 1, 100)
 
-finish = datetime.now()
+xgrid, ygrid = np.meshgrid(x, y)
+zgrid = 1 / np.square(xgrid * ygrid)
 
-print(start, finish)
+
+fig = plt.figure("Eye of Sauron")
+ax = fig.gca(projection="3d")
+ax.set_axis_off()
+surface = ax.plot_surface(zgrid, ygrid, xgrid, cmap=cm.hsv)
+fig.show()
+while True:
+    horizontal, vertical = get_angles()
+    ax.view_init(horizontal, vertical)
+    plt.draw()
+    plt.pause(0.01)
